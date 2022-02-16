163c79e28e45ef9dc7392fe6857449b6b2c7343b
eunchannam <namhm23@gmail.com>
2022-02-16 22:34:18 +0900

    no change

--- src/main/java/cteam/cteamproject/web/member/controller/MemberController.java
+++ src/main/java/cteam/cteamproject/web/member/controller/MemberController.java
@@ -28,7 +28,7 @@ public class MemberController {
 
     @GetMapping("/my-info")
     public Member myInfo(@SessionAttribute(name = "loginMember") Long memberId) {
-        log.info("본인 멤버 조회");
+        log.info("본인 정보 조회");
         return memberService.findById(memberId);
     }
 
